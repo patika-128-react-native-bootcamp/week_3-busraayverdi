84a400b1df7410daced4a53b886a5b1eb27a4307
busraayverdi <busraayverdi.95@gmail.com>
2021-12-08 22:24:40 +0300

    commit

--- src/pages/Product/ProductDetail/ProductDetail.js
+++ src/pages/Product/ProductDetail/ProductDetail.js
@@ -23,10 +23,10 @@ export default function ProductDetail() {
           {product.isPopular && <Icon name="star" color="orange" size={25} />}
         </View>
         <ScrollView horizontal bounces={false}>
-          {product.ingredients.map((ing, ind) => {
+          {product.ingredients.map((ingredients, index) => {
             return (
-              <View style={styles.badge_container}>
-                <Text style={styles.badge_label}>{ing}</Text>
+              <View key={index} style={styles.badge_container}>
+                <Text style={styles.badge_label}>{ingredients}</Text>
               </View>
             );
           })}
